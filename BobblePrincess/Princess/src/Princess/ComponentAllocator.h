bf2eab344ff7ab49daec0c99368310c38fde5b9a
PaulineVH <vandenheede.pauline@gmail.com>
2020-06-10 00:32:07 +0200

    [MOD] - ComponentAllocator - Find Function
    Find function works with pointers

--- BobblePrincess/Princess/src/Princess/ComponentAllocator.h
+++ BobblePrincess/Princess/src/Princess/ComponentAllocator.h
@@ -1,21 +1,21 @@
 #pragma once
-#include "FixedSizeAllocator.h"
+//Project includes
 #include "Utils.h"
+#include "Components.h"
+#include "Exception.h"
+#include "Core.h"
 
-
+//Standard includes
 #include <memory>
 #include <iostream>
 
-
 namespace Princess
 {
-	/*
-		The next pointer is stored in the component itself.
-	*/
 	template<typename T>
 	class ComponentAllocator
 	{
 	public:
+		//------ Public Templated Functions ------
 		//---- Constructors ----
 		ComponentAllocator();
 		ComponentAllocator(const uint16_t nrOfBlocks);
@@ -25,36 +25,37 @@ namespace Princess
 		~ComponentAllocator();
 		//---- Operators ----
 		//-- Access Operators --
-		T* operator[](uint16_t index);
-		const T* operator[](uint16_t index) const;
+		T& operator[](uint16_t index);
+		const T& operator[](uint16_t index) const;
 		//-- Assignment Operators --
 		ComponentAllocator& operator=(const ComponentAllocator&) = delete;
 		ComponentAllocator& operator=(ComponentAllocator&&) = delete;
 
 		//---- Functionality ----
+		void Initialise(uint16_t nrOfBlocks);
 		uint16_t Add(T&& value);
+		T* Find(uint16_t entityID) const;
 		//remove from the list and swap with the last one
 		void Remove(uint16_t id);
 
 	private:
 		//---- Datamembers ----
-		const uint16_t m_Capacity;
-
 		T* m_pHead;
+		
+		uint16_t m_Capacity;
 		uint16_t m_Size;
 	};
 
-
-
-	
 }
 
 
-//-------- Templated Functions --------
+//------ Public Templated Functions ------
 //---- Constructors ----
-template<typename T>
-inline Princess::ComponentAllocator<T>::ComponentAllocator()
-	: ComponentAllocator( 10 )
+template < typename T>
+Princess::ComponentAllocator<T>::ComponentAllocator()
+	: m_Capacity{ 0 }
+	, m_Size{ 0 }
+	, m_pHead{ nullptr }
 {
 }
 template<typename T>
@@ -79,23 +80,26 @@ Princess::ComponentAllocator<T>::~ComponentAllocator()
 //---- Operators ----
 //-- Access Operators --
 template<typename T>
-T* Princess::ComponentAllocator<T>::operator[](uint16_t index)
+inline T& Princess::ComponentAllocator<T>::operator[](uint16_t index)
 {
-	if (index >= m_Size)
-		throw(std::exception("Out of bounds."));
-
-	return m_pHead + index;
+	return *(m_pHead + index - 1);
 }
 template<typename T>
-const T* Princess::ComponentAllocator<T>::operator[](uint16_t index) const
+inline const T& Princess::ComponentAllocator<T>::operator[](uint16_t index) const
 {
-	if (index >= m_Size)
-		throw(std::exception("Out of bounds."));
-
-	return m_pHead + index;
+	return *(m_pHead + index - 1);
 }
+//-- Assignment Operators --
+//DELETED
 
 //---- Functionality ----
+template<typename T>
+void Princess::ComponentAllocator<T>::Initialise(uint16_t nrOfBlocks)
+{
+	std::cout << "Component Allocator inialising with number " << nrOfBlocks << " and size " << sizeof(T) << " !\n";
+	m_pHead = reinterpret_cast<T*>(calloc(static_cast<size_t>(nrOfBlocks), sizeof(T)));
+	m_Capacity = nrOfBlocks;
+}
 template<typename T>
 uint16_t Princess::ComponentAllocator<T>::Add(T&& value)
 {
@@ -106,23 +110,33 @@ uint16_t Princess::ComponentAllocator<T>::Add(T&& value)
 	++m_Size;
 	return m_Size; //index that you return is from 1 - max 
 }
-
+//Returns pointer because this function doesn't necessarily find a component with the entityID given as variable.
 template<typename T>
-void Princess::ComponentAllocator<T>::Remove(uint16_t id)
+T* Princess::ComponentAllocator<T>::Find(uint16_t entityID) const
 {
-	if (id > m_Size)
-		return;
-	/*
-		not putting to zero memory makes it the case that you need to initilaise the value
-	*/
-	if (id == m_Size - 1)
-	{
-		--m_Size;
-		return;
+	T* pBegin{ m_pHead };
+	// if m_Size == 1 then space 0 is filled in.
+	T* pEnd{ m_pHead + m_Size - 1 };
+
+	for (; pBegin != pEnd; ++pBegin) {
+		if (static_cast<Princess::BaseComponent>(pBegin).IDentity == entityID) {
+			return *pBegin;
+		}
 	}
-	/*
-		move last one to id space
-	*/
-	*(m_pHead + id - 1) = std::move(*(m_pHead + m_Size - 1));
-	--m_Size;
+
+	return nullptr;
+}
+template<typename T>
+void Princess::ComponentAllocator<T>::Remove(uint16_t entityID)
+{
+	T* pFound = Find(entityID);
+	if (pFound) // if nullptr then this statement is false
+	{
+		//move last one to space that needs to be removed except if it's the last one in the array + decrement size
+		if (pFound != m_pHead + m_Size - 1)
+		{
+			*(pFound) = std::move(*(m_pHead + m_Size - 1));
+		}
+		--m_Size;
+	}
 }
